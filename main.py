12da6f6788b303ab1ba998f503a1c4601bff590f
Jason0411202 <Jason0411202@gmail.com>
2024-07-18 02:10:25 +0800

    init

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import subprocess
+import cv2
+import dlib
+from PIL import Image, ImageEnhance
+
+detector = dlib.get_frontal_face_detector() # 載入 dlib 的臉部檢測器
+
+def ClearDirectory(directory):
+    # 先清空 "Real-ESRGAN/inputs 目錄下的所有檔案與資料夾
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.remove(file_path)  # 刪除檔案或符號連結
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)  # 刪除資料夾
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+def Detect_and_crop_face(output_size):
+    file_names = os.listdir("input")
+    file_names = [f for f in file_names if os.path.isfile(os.path.join("input", f))]
+    image_path = os.path.join("input", file_names[0])
+
+    img = cv2.imread(image_path)     # 讀取圖片
+    if img is None:
+        print("Image not found!")
+        return
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 轉成灰階
+    faces = detector(gray) # 檢測人臉
+    if len(faces) == 0:
+        print("No face detected!") 
+        return
+    
+    face = faces[0] # 只處理第一個檢測到的人臉
+    x, y, w, h = (face.left(), face.top(), face.width(), face.height()) # 計算人臉區域
+    padding = int(0.5 * h)  # 擴展高度的一半作為以容納軀幹
+    cropped_img = img[max(0, y-padding):min(img.shape[0], y+h+padding), max(0, x-padding):min(img.shape[1], x+w+padding)] # 裁剪人臉區域
+    
+    pil_img = Image.fromarray(cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB))  # 轉換為PIL圖片
+    pil_img = pil_img.resize(output_size, Image.Resampling.LANCZOS) # 調整圖片大小
+    enhancer = ImageEnhance.Sharpness(pil_img) # 簡單的畫質增強
+    pil_img = enhancer.enhance(2.0)  # 增強銳度
+
+    ClearDirectory('Real-ESRGAN/inputs')  # 先清空上一次的輸出目錄
+
+    output_path = "Real-ESRGAN/inputs/image.jpg" # 儲存結果
+    pil_img.save("Real-ESRGAN/inputs/image.jpg")
+    print(f"Processed image saved as {output_path}")
+
+def EnhanceImage():
+    command = [
+        'python',
+        'Real-ESRGAN/inference_realesrgan.py',
+        '-n', 'RealESRGAN_x4plus',
+        '-i', 'Real-ESRGAN/inputs',
+        '--face_enhance'
+    ]
+
+    # 執行命令
+    result = subprocess.run(command, capture_output=True, text=True)
+    print("Standard Output:\n", result.stdout)
+    print("Standard Error:\n", result.stderr)
+
+def ScalingImage(output_size):
+    # 讀取圖片
+    img = cv2.imread('Real-ESRGAN/results/image.jpg')
+
+    # 縮放圖片到 500x500
+    resized_img = cv2.resize(img, (500, 500))
+
+    # 保存縮放後的圖片
+    cv2.imwrite('output/image.jpg', resized_img)
+
+# 調用函數，設置輸出大小為 n*m (例如：200x300)
+Detect_and_crop_face((500, 500))
+EnhanceImage()
+ScalingImage((500, 500))
+
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+# 環境配置
+* 環境: python 3.11
+* 註: win10 安裝 dlib   https://github.com/z-mahmud22/Dlib_Windows_Python3.x
+
+```
+conda create -n real-esrgan python=3.11
+conda activate real-esrgan
+git clone https://github.com/xinntao/Real-ESRGAN.git
+cd Real-ESRGAN
+pip install --verbose basicsr --use-pep517
+pip install facexlib
+pip install gfpgan
+pip install -r requirements.txt
+python setup.py develop
+wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models
+```
+
+## 測試 real-esrgan 是否正常運作
+在 Real-ESRGAN 專案根目錄下執行以下指令
+```bash
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs --face_enhance
+```
+
+# 執行
+輸入的圖片放在 input 資料夾中
+圖片將會輸出在 output 資料夾中
+```bash
+python main.py
+```
